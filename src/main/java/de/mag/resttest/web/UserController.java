ba643baaa85f7e4e8fb9670c92d25e2708149361
marco <m.gablowsky@googlemail.com>
2013-11-05 22:21:21 +0100

    implemented UserController and corresponding tests

---
+++ src/main/java/de/mag/resttest/web/UserController.java
@@ -0,0 +1,51 @@
+package de.mag.resttest.web;
+
+import java.util.Collection;
+
+import javax.annotation.Resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import de.mag.resttest.app.users.User;
+import de.mag.resttest.app.users.UserService;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+	@Resource
+	private UserService userService;
+
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.GET)
+	public Collection<User> getUsers() {
+		return userService.findAll();
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	public User getUser(@PathVariable Long userId) {
+		return userService.findById(userId);
+	}
+
+	@ResponseStatus(value = HttpStatus.CREATED)
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.POST)
+	public Long createUser(@RequestBody User user) {
+		return userService.create(user);
+	}
+
+	@ResponseStatus(value = HttpStatus.OK)
+	@RequestMapping(value = "/{userId}", method = RequestMethod.PUT)
+	public void updateUser(@RequestBody User user) {
+		userService.update(user);
+	}
+
+}
---
+++ src/test/java/de/mag/resttest/web/UserControllerTest.java
@@ -0,0 +1,61 @@
+package de.mag.resttest.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.testng.annotations.Test;
+
+import de.mag.resttest.AbstractWebIntegrationTest;
+
+public class UserControllerTest extends AbstractWebIntegrationTest {
+
+	@Test
+	public void listsAllUsers() throws Exception {
+		mvc.perform(
+				get("/users"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(content().encoding("UTF-8"));
+	}
+
+	@Test
+	public void createsUser() throws Exception {
+		try (InputStream in = new FileInputStream("src/test/resources/createUser.json")) {
+			mvc.perform(
+					post("/users")
+							.contentType(MediaType.APPLICATION_JSON)
+							.content(IOUtils.toByteArray(in)))
+					.andExpect(status().isCreated())
+					.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+					.andExpect(content().encoding("UTF-8"));
+		}
+	}
+
+	@Test
+	public void deliversUserById() throws Exception {
+		mvc.perform(
+				get("/users/1"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(content().encoding("UTF-8"));;
+	}
+
+	@Test
+	public void updatesUser() throws Exception {
+		try (InputStream in = new FileInputStream("src/test/resources/createUser.json")) {
+			mvc.perform(
+					put("/users/1")
+							.contentType(MediaType.APPLICATION_JSON)
+							.content(IOUtils.toByteArray(in)))
+					.andExpect(status().isOk());
+		}
+	}
+}
